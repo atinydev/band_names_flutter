0f0c942d8843edb4ddcdff981cd4f21ac349abb8
atinydev <atinydeveloper@gmail.com>
2022-03-24 13:55:31 -0600

    Create Band Model

---
+++ lib/models/band.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+class Band {
+  final String id;
+  final String name;
+  final int votes;
+
+  const Band({
+    required this.id,
+    required this.name,
+    required this.votes,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'votes': votes,
+    };
+  }
+
+  factory Band.fromMap(Map<String, dynamic> map) {
+    return Band(
+      id: map['id'] ?? '',
+      name: map['name'] ?? '',
+      votes: map['votes']?.toInt() ?? 0,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Band.fromJson(String source) => Band.fromMap(json.decode(source));
+}
